d49fc4f203b5f2255e7235ba1081e51b653cee88
Higor de Deus <higordeus22@gmail.com>
2023-07-18 19:26:41 -0300

    Ocelot implementado

---
+++ GuardianBackend.Common/AutoDIAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuardianBackend.Common
+{
+    [AttributeUsage(AttributeTargets.Interface)]
+    public class AutoDIAttribute : Attribute { }
+}
--- GuardianBackend/Program.cs
+++ GuardianBackend/Program.cs
@@ -1,22 +1,28 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using NLog.Extensions.Logging;
+using Microsoft.Extensions.Logging;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Adicione os serviços ao contêiner.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Saiba mais sobre a configuração do Swagger/OpenAPI em https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot(builder.Configuration);
 
-// NLog configuration
+// Configuração do NLog
 builder.Logging.ClearProviders();
 builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
 builder.Logging.AddNLog();
 
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configura o pipeline de solicitações HTTP.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -29,4 +35,13 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+// Adicione o Ocelot ao pipeline de middleware
+app.UseOcelot().Wait();
+
+// Cria o logger
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+logger.LogInformation("O aplicativo foi iniciado.");
+
 app.Run();
+
+logger.LogInformation("O aplicativo está sendo encerrado.");
